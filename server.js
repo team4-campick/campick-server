e70b0344d74173afbd2501ef47cb60260327d0be
leedbswo107 <ljyu1@naver.com>
2024-06-19 13:16:04 +0900

    feat: 몽구스 셋팅 & 마이페이지 route 설정

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const myPageRoute = require('./routes/myPageRoute');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT_NUM;
-
-app.use(cors({ credentials: true, origin: `http://localhost:${port}` }));
+const connectUri = process.env.DB_KEYS;
+const clientPort = process.env.CLIENT_PORT_NUM;
+app.use(cors({ credentials: true, origin: `http://localhost:${clientPort}` }));
 app.use(express.json());
+app.use('/', myPageRoute);
+mongoose.connect(connectUri);
 
 app.get('/', async (req, res) => res.json('Hello World!'));
 
