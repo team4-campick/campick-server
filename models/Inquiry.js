b7d1bc780e53dde52dde1e5ec8c1ae0c473d2396
leedbswo107 <ljyu1@naver.com>
2024-06-20 17:55:58 +0900

    feat: 스키마 타임 정보 추가

--- models/Inquiry.js
+++ models/Inquiry.js
@@ -5,7 +5,7 @@ const inquirySchema = new mongoose.Schema({
   email: { type: String, required: true },
   content: { type: String, required: true },
 });
-
+inquirySchema.set('timestamps', true);
 const Inquiry = mongoose.model('Inquiry', inquirySchema);
 
 module.exports = Inquiry;
