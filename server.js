a9bf3822247e5a1414c435c8fce895e733ebd4e3
leedbswo107 <ljyu1@naver.com>
2024-06-21 20:37:50 +0900

    feat: conflict resolve & duplicate line delete

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const myPageRoute = require("./routes/myPageRoute");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const salePostRoutes = require('./routes/salePostRoutes');
+const myPageRoute = require('./routes/myPageRoute');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT_NUM;
@@ -11,23 +12,22 @@ const connectUri = process.env.DB_KEYS;
 const clientPort = process.env.CLIENT_PORT_NUM;
 app.use(cors({ credentials: true, origin: `http://localhost:${clientPort}` }));
 app.use(express.json());
-app.use("/", myPageRoute);
-app.use("/", myPageRoute);
-app.use("/api/sale-posts", salePostRoutes);
+app.use('/api/sale-posts', salePostRoutes);
 
-app.use("/", myPageRoute);
+app.use('/', myPageRoute);
 mongoose.connect(connectUri);
 
-app.get("/", async (req, res) => res.json("Hello World!"));
+app.get('/', async (req, res) => res.json('Hello World!'));
 
-if (username && password && nickname) {
-  res.json({ id: 1, message: "회원가입 성공" });
-} else {
-  res.status(400).json({ message: "회원가입 실패: 필수 항목을 입력하세요." });
-}
+// 아래 코드는 controller 혹은 service 단에 들어가야합니다.
+// if (username && password && nickname) {
+//   res.json({ id: 1, message: '회원가입 성공' });
+// } else {
+//   res.status(400).json({ message: '회원가입 실패: 필수 항목을 입력하세요.' });
+// }
 
 // 라우터 설정
-const registerRouter = require("./routes/register");
-app.use("/register", registerRouter);
+const registerRouter = require('./routes/register');
+app.use('/register', registerRouter);
 
 app.listen(port, () => console.log(`${port}번에서 돌아감`));
