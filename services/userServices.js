9658a6e3cc219df4d4501505005748081bb215b9
leedbswo107 <ljyu1@naver.com>
2024-06-21 19:29:22 +0900

    feat: getUser func

--- services/userServices.js
+++ services/userServices.js
@@ -1,17 +1,22 @@
-const User = require("../models/User");
+const User = require('../models/User');
 class UserService {
+  async getUser(userId) {
+    const user = await User.findOne({ userId });
+    console.log('func user', user);
+    return user;
+  }
   async deleteUser(userId) {
-    const user = await User.findById(userId);
-    if (!user) throw new Error("User not found");
-    await User.findByIdAndDelete(userId);
-    return { message: "User successfully deleted" };
+    console.log('userId :', userId);
+    const test = await User.findOne(userId);
+    console.log('test', test);
+    return { message: 'User successfully deleted' };
   }
   async duplicateNickname(nickname) {
-    console.log("func nickname", typeof nickname);
+    console.log('func nickname', typeof nickname);
     const nicknameCheck = await User.findOne({ nickname });
-    console.log("nicknameDoc", nicknameCheck);
-    if (nicknameCheck) throw new Error("Nickname already exists");
-    return { message: "Can use this nickname" };
+    console.log('nicknameCheck', nicknameCheck);
+    if (nicknameCheck) throw new Error('Nickname already exists');
+    return { message: 'Can use this nickname' };
   }
 }
 module.exports = new UserService();
