99b2c72bc386bd8d67f657214112bf63d9c66171
leedbswo107 <ljyu1@naver.com>
2024-06-24 09:44:31 +0900

    feat: Schema test

--- models/User.js
+++ models/User.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: {
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
     type: String,
     required: true,
   },
   nickname: {
     type: String,
     required: true,
-    unique: true,
+    // unique: true,
   },
-  pw: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  loginDate: {
-    type: Object,
-    required: true,
-    // time form "2011-10-05T14:48:00.000Z"
-  },
+
+  // loginDate: {
+  //   type: Object,
+  //   required: true,
+  // },
+  // time form "2011-10-05T14:48:00.000Z"
 });
 const User = mongoose.model('User', userSchema);
 
