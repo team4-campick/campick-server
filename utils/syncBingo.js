30a99c6a24e724d909fbdd65d71a8fa0e5662bd2
leedbswo107 <ljyu1@naver.com>
2024-06-25 18:05:04 +0900

    feat: syncBingo func

---
+++ utils/syncBingo.js
@@ -0,0 +1,13 @@
+exports.syncBingo = (curBingo, newBingo) => {
+  console.log('syncBingo', curBingo.bingo);
+  console.log('syncBingo', newBingo);
+  Array.from(curBingo.bingo).forEach((e) => {
+    newBingo.forEach((element) => {
+      if (e.mission === element.mission) {
+        [e.state, element.state] = [element.state, e.state];
+      }
+    });
+  });
+  console.log('syncBingo', curBingo);
+  return curBingo;
+};
