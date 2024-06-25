f3b1e21b0803e441a69a58c4f695a38ea29ea1b0
leedbswo107 <ljyu1@naver.com>
2024-06-25 18:00:10 +0900

    feat: get review & post count

--- services/bingoService.js
+++ services/bingoService.js
@@ -3,7 +3,7 @@ const Mission = require('../models/Mission');
 const Review = require('../models/Review');
 const Post = require('../models/Post');
 const Bingo = require('../models/Bingo');
-const { default: bingoRule } = require('../utils/bingoRule');
+const { bingoRule } = require('../utils/bingoRule');
 const { missionClearCounter } = require('../utils/missionClearCounter');
 
 class BingoService {
@@ -18,28 +18,39 @@ class BingoService {
   }
   // ================== mission variable status handling =================== //
   async getReviewCount(_id) {
-    const review = await Review.findOne({ _id });
-    const reviewCount = review ? review.length : 0;
-    return reviewCount;
+    const review = (await Review.findOne({ _id }))
+      ? await Review.findOne({ _id })
+      : null;
+    console.log('리뷰 카운터', _id); //
+    console.log('리뷰 카운터 리뷰 리스트', review);
+    if (review) {
+      const reviewCount = review.review ? review.review.length : 0;
+      console.log('리뷰 갯수', reviewCount);
+      return reviewCount;
+    } else {
+      return 0;
+    }
   }
   async getPostCount(_id) {
-    const post = await Post.findOne({ _id });
+    const post = (await Post.findOne({ _id }))
+      ? await Post.findOne({ _id })
+      : null;
     const postCount = post ? post.length : 0;
     return postCount;
   }
   async getMissionClear(_id) {
     const bingo = await Bingo.findOne({ _id });
     const count = missionClearCounter(bingo);
+    console.log('클리어한 미션 :', count);
     return count;
   }
   async getBingoCount(_id) {
-    const bingo = await Bingo.findOne({ _id });
-    const count = bingoRule(bingo);
+    const getBingo = await Bingo.findOne({ _id });
+    const count = bingoRule(getBingo.bingo);
     return count;
   }
   async getContinuousConnection(_id) {
     const user = await User.findOne({ _id });
-    // console.log('user : ', user);
     return user;
   }
   // ====================== mission variable status handling ===================
