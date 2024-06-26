d3b5b9630e48ba8f69a427c2313b44cb37efb59b
leedbswo107 <ljyu1@naver.com>
2024-06-26 17:42:28 +0900

    feat: bingo area route set

--- routes/myPageRoute.js
+++ routes/myPageRoute.js
@@ -12,7 +12,9 @@ router.post('/passwordCheck/:id', myPageController.passwordCheck);
 router.post('/editInfo/:id', myPageController.updateUser);
 
 // router.get('/get-bingo/:id', myPageController.getBingo);
-router.post('/bingo-count/:id', myPageController.getBingoCount);
+router.get('/bingo-count/:id', myPageController.getBingoCount);
+router.get('/bingo-pattern/:id', myPageController.getBingoPattern);
 router.post('/update-mission/:id', myPageController.updateMission);
+// router.post('/reset-bingo/:id', myPageController.bingoStatusReset)
 
 module.exports = router;
