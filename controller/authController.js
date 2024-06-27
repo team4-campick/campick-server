87e377c523b92aba7ded1035b7c6c4a110a23a34
kim soojin <verysoojini@gmail.com>
2024-06-27 17:52:30 +0900

    add : 토큰 할당 로직 추가

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 // controller/authController.js
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const users = []; // 임시로 메모리에 사용자 데이터를 저장합니다.
 
@@ -11,13 +11,13 @@ const register = async (req, res) => {
 
   // 사용자 유효성 검사
   if (!username || !password || !nickname) {
-    return res.status(400).json({ message: '모든 필드를 입력하세요.' });
+    return res.status(400).json({ message: "모든 필드를 입력하세요." });
   }
 
   // 이미 존재하는 사용자 체크
   const existingUser = await User.findOne({ username });
   if (existingUser) {
-    return res.status(400).json({ message: '이미 존재하는 사용자입니다.' });
+    return res.status(400).json({ message: "이미 존재하는 사용자입니다." });
   }
 
   // 비밀번호 해싱
@@ -29,7 +29,7 @@ const register = async (req, res) => {
 
   res.status(201).json({
     id: newUser._id,
-    message: '회원가입이 성공적으로 완료되었습니다.',
+    message: "회원가입이 성공적으로 완료되었습니다.",
   });
 };
 
@@ -42,16 +42,16 @@ const login = async (req, res) => {
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(404).json({ message: 'nouser' });
+      return res.status(404).json({ message: "nouser" });
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      return res.status(400).json({ message: 'failed' });
+      return res.status(400).json({ message: "failed" });
     }
 
-    const token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
-      expiresIn: '1h',
+    const token = jwt.sign({ id: user._id }, "your_jwt_secret", {
+      expiresIn: "1h",
     });
     await User.findByIdAndUpdate(
       user,
@@ -60,8 +60,8 @@ const login = async (req, res) => {
     );
     return res.json({ id: user._id, token });
   } catch (error) {
-    console.error('Error logging in:', error);
-    return res.status(500).json({ message: 'Internal server error' });
+    console.error("Error logging in:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
