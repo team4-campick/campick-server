3358cbb943e1629fcd4610acc4cf60f0f3964a63
leedbswo107 <ljyu1@naver.com>
2024-06-28 14:02:06 +0900

    feat: route set

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,16 @@
 // routes/authRoutes.js
 const express = require("express");
-const { register, login } = require("../controller/authController");
-
 const router = express.Router();
+const {
+  register,
+  login,
+  logout,
+  profile,
+} = require("../controller/authController");
 
 // router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
+router.get("/profile", profile);
 
 module.exports = router;
