54869ec7e3219367fb9021f84ca4a2b02b92b0e9
leedbswo107 <ljyu1@naver.com>
2024-07-03 03:54:00 +0900

    add: campsite route setting

--- routes/campsiteRoutes.js
+++ routes/campsiteRoutes.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const campsiteController = require("../controllers/campsitesController");
 
-router.get("/get-sites", campsiteController.getSites);
+const campsiteController = require("../controller/campsiteController");
+
+router.get("/get-reviews/:id", campsiteController.getReviews);
+router.post("/create-review/:id", campsiteController.createReview);
+router.get("/user/:id", campsiteController.getUser);
+router.delete("/delete/:id", campsiteController.deleteReview);
+router.post("/edit-review/:id", campsiteController.editReview);
+
+module.exports = router;
