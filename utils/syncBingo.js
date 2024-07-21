9f02eefc9a15b549479d3f05404f22e4e378613a
leedbswo107 <ljyu1@naver.com>
2024-07-21 21:40:38 +0900

    refactor: remove unnecessary console output

--- utils/syncBingo.js
+++ utils/syncBingo.js
@@ -1,6 +1,4 @@
 exports.syncBingo = (curBingo, newBingo) => {
-  console.log("syncBingo", curBingo.bingo);
-  console.log("syncBingo", newBingo);
   Array.from(curBingo.bingo).forEach((e) => {
     newBingo.forEach((element) => {
       if (e.mission === element.mission) {
@@ -8,6 +6,5 @@ exports.syncBingo = (curBingo, newBingo) => {
       }
     });
   });
-  console.log("syncBingo", curBingo);
   return curBingo;
 };
