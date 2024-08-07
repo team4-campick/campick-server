b2865684e8ae379c9ce2702b40d71929ee9ca50d
leedbswo107 <ljyu1@naver.com>
2024-08-07 23:16:42 +0900

    remove: test file

--- routes/testRegisterRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const registerTestController = require("../controller/registerTestController");
-
-// router.post("/register", registerTestController.registerUser);
-
-module.exports = router;
